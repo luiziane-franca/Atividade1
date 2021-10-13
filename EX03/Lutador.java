351390a2b194a0f49ac7b2ac9a3c3fdc214f6f0c
luiziane-franca <89031349+luiziane-franca@users.noreply.github.com>
2021-10-13 19:57:17 -0300

    Add files via upload

---
+++ EX03/Lutador.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package luiza08102021_03;
+
+/**
+ *
+ * @author Nid
+ */
+public class Lutador {
+    String nome;
+    int energia;
+    int forca;
+    public Lutador(String nome, int energia, int forca){
+        this.nome = nome;
+        this.energia = energia;
+        this.forca = forca;
+    }
+    
+    public void aplicarGolpe(Lutador oponente){
+        oponente.energia -= forca;
+    }
+}
